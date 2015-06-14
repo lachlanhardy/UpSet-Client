93eaed16a911a8498a03c6e0db0a5c327b36dd1a
Lachlan Hardy <lachlan@neo.com>
2015-06-14 20:26:54 +1000

    Replaced Raphael with PNGs.

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 (function () {
 
+  var fake = [{"pattern":"solid","colour":"purple","number":"two","shape":"tilde"},{"pattern":"stripe","colour":"purple","number":"three","shape":"oval"},{"pattern":"empty","colour":"green","number":"one","shape":"diamond"},{"pattern":"stripe","colour":"red","number":"two","shape":"diamond"},{"pattern":"solid","colour":"red","number":"two","shape":"tilde"},{"pattern":"stripe","colour":"purple","number":"two","shape":"diamond"},{"pattern":"stripe","colour":"red","number":"three","shape":"diamond"},{"pattern":"solid","colour":"green","number":"one","shape":"tilde"},{"pattern":"empty","colour":"purple","number":"one","shape":"oval"},{"pattern":"empty","colour":"red","number":"three","shape":"oval"},{"pattern":"stripe","colour":"green","number":"three","shape":"diamond"},{"pattern":"empty","colour":"red","number":"one","shape":"tilde"}];
+
   upset = {
     setup: function () {
-      upset.getData();
+      upset.login();
+      // upset.draw(fake);
     },
 
     queryURL: "http://10.0.1.81:9292/grid",
     postURL: "http://10.0.1.81:9292/guess",
+    loginURL: "http://10.0.1.81:9292/login",
+    meURL: "http://10.0.1.81:9292/me",
+
+    login: function () {
+      $.post(upset.loginURL, {
+        user_name: 'foo'
+      }).done(function () {
+        upset.getData();
+      });
+    },
 
     getData: function () {
       $.ajax({
@@ -26,8 +39,6 @@
   upset.card = upset.card || {};
   upset.card = {
     display: function(data){
-      console.log("drawing a card");
-
       var cards = data,
           $grid = $('.grid');
 
@@ -38,117 +49,76 @@
     },
 
     createCard: function(card) {
-      card.width = 150;
-      card.height = 250;
-      card.r = Raphael(0, 0, card.width, card.height);
+      var $card = $("<card>"),
+          imageName = upset.dimensions.shape(card) + "-" + upset.dimensions.pattern(card) + "-fff.png";
 
-      var cardBacking = card.r.rect(0, 0, card.width, card.height, 15)
-                     .attr({
-                      fill: "hsb(0, 0, 0)",
-                      stroke: "none",
-                      opacity: 0.1
-                     });
+      for (var i = upset.dimensions.number(card); i > 0; i--) {
+        $card.append("<img src='railsset-icons/" + imageName + "' style='background: "+ upset.dimensions.colour(card) + "'/>")
+      };
 
-      upset.utils.pattern(card);
-
-      var symbol = card.r.rect(upset.utils.centrePoint(card.width), upset.utils.centrePoint(card.height), 10, 10, 0)
-                    .attr({
-                      fill: upset.utils.colour(card.colour),
-                      stroke: "none",
-                      opacity: 1
-                     });
-
-      var oval = card.r.ellipse(upset.utils.centrePoint(card.width), upset.utils.centrePoint(card.height), 30, 50)
-                  .attr({
-                    fill: upset.utils.colour(card.colour),
-                    stroke: "none",
-                    opacity: 1
-                   });
-
-      console.log(card.r);
-
-      var $card = $("<card>");
-      return $card.append(card.r.canvas);
+      return $card.append(card);
     }
 
   };
 
-
-
-  upset.utils = upset.utils || {};
-  upset.utils = {
-    centrePoint: function (value) {
-      return (value / 2)
-    },
+  upset.dimensions = upset.dimensions || {};
+  upset.dimensions = {
 
     pattern: function (card) {
       switch (card.pattern) {
         case "solid":
-          var solid = card.r.rect(0, 0, card.width, card.height, 15)
-               .attr({
-                fill: upset.utils.colour(card.colour),
-                stroke: "none",
-                opacity: 0.5
-               });
+          return "solid"
           break
         case "stripe":
-          var loop = 10;
-          for (var i = 0; i < loop; i++) {
-            card.r.path("M" + i + "0 " + 100 + " L" + 100 + " " + i + "0")
-                  .attr({
-                    stroke: upset.utils.colour(card.colour),
-                    "stroke-width": 2,
-                    opacity: 0.5
-                  });
-          };
+          return "pattern"
           break
         case "empty":
-          // empty needs no love
+          return "outline"
           break
       };
     },
 
-    shape: function(shape){
-      switch (shape) {
+    shape: function(card){
+      switch (card.shape) {
         case "oval":
-          console.log("this is an oval");
+          return "fire"
           break
         case "diamond":
-          console.log("this is a diamond");
+          return "ruby"
           break
         case "tilde":
-          console.log("this is a tilde");
+          return "boat"
           break
       };
     },   
 
-    number: function(number){
-      switch (number) {
+    number: function(card){
+      switch (card.number) {
         case "one":
-          console.log("There is one symbol");
+          return 1
           break
         case "two":
-          console.log("there are two symbols");
+          return 2
           break
         case "three":
-          console.log("there are three symbols");
+          return 3
           break
       };
     },
 
-    colour: function(colour){
-      switch (colour) {
+    colour: function(card){
+      switch (card.colour) {
         case "green":
-          colour = "120";
+          colour = "229, 148, 37";
           break;
         case "red":
-          colour = "0";
+          colour = "217, 52, 147";
           break;
         case "purple":
-          colour = "290";
+          colour = "52, 69, 147";
           break;
       };
-      return "hsb(" + colour + ", 90, 100)"
+      return "rgb(" + colour + ")"
     }
 
   };
